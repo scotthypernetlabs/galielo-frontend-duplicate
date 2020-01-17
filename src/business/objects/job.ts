d5c829ca3d3f753c854f4107021be1c932dd9abd
Tran Nguyen <trandnnguyen@gmail.com>
2020-01-16 16:08:38 -0800

    Jobs API up to date with server

--- src/business/objects/job.ts
+++ src/business/objects/job.ts
@@ -1,14 +1,17 @@
 import { Dictionary } from './dictionary';
+import DateTimeFormat = Intl.DateTimeFormat;
 
 export class Job {
-  readonly path: string;
-  readonly name: string;
-  readonly launch_pad: string;
-  readonly landing_zone: string;
-  readonly id: string;
-  readonly run_time: number;
-  readonly upload_time: number;
-  readonly status: string;
+  constructor(
+    readonly path: string,
+    readonly name: string,
+    readonly launch_pad: string,
+    readonly landing_zone: string,
+    readonly id: string,
+    readonly run_time: number,
+    readonly upload_time: DateTimeFormat,
+    readonly status: EJobStatus,
+  ) {}
 }
 
 export class JobStatusHistory {
@@ -18,4 +21,41 @@ export class JobStatusHistory {
 export class JobStatus {
   readonly status: string;
   readonly timestamp: number;
+  readonly jobstatusid?: string;
+  readonly jobid?: string;
+}
+
+export enum EJobStatus {
+  uploaded,
+  submitted,
+  downloaded,
+  building_image,
+  building_container,
+  start_requested,
+  running,
+  pause_requested,
+  paused,
+  stop_requested,
+  stopped,
+  exited,
+  collecting_results,
+  posting_results,
+  terminated,
+  completed,
+  removed_by_host,
+  unknown,
+  post_processing,
+  started
+}
+
+export enum EJobRunningStatus {
+  not_running,
+  running
+}
+
+export enum EPaymentStatus {
+  current,
+  payment_due,
+  delinquent,
+  missing_offer
 }
