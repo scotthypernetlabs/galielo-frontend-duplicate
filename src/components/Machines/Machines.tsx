782a9c2e8ea19aee27551522d1ef6fd272c0dbe7
Tran Nguyen <trandnnguyen@gmail.com>
2020-01-20 15:33:51 -0800

    My Machines displays under Machines tab

--- src/components/Machines/Machines.tsx
+++ src/components/Machines/Machines.tsx
@@ -6,6 +6,9 @@ import { User } from '../../business/objects/user';
 import { Machine } from '../../business/objects/machine';
 import { openNotificationModal } from '../../actions/modalActions';
 import LandingZone from './LandingZone';
+import {MyContext} from "../../MyContext";
+import {context} from "../../context";
+import {MachineRepository} from "../../data/implementations/machineRepository";
 
 type Props = {
   currentUser: User;
@@ -13,20 +16,37 @@ type Props = {
 };
 
 type State = {
-
+  currentUserMachines: Machine[];
 }
 
 class Machines extends React.Component<Props, State> {
+  context!: MyContext;
   constructor(props: Props){
     super(props);
+    this.state = {
+      currentUserMachines: undefined
+    };
     this.addMachine = this.addMachine.bind(this);
   }
+
+  componentDidMount(): void {
+    this.context.machineRepository.getMachines()
+      .then((response) => {
+        console.log('response', response);
+        this.setState({currentUserMachines: response});
+      });
+  }
+
   public addMachine(e:any){
     e.preventDefault();
     this.props.openNotificationModal('This feature will be added in the future!');
   }
   public render(){
-    const currentUserMachines:Machine[] = [];
+    if(!this.state.currentUserMachines) {
+      return <div/>
+    }
+    const { currentUserMachines } = this.state;
+
     return(
       <div className="stations-container">
         <div className="stations-header">
@@ -52,15 +72,17 @@ class Machines extends React.Component<Props, State> {
   }
 }
 
+Machines.contextType = context;
+
 const mapStateToProps = (state: IStore) => ({
   currentUser: state.users.currentUser
   // currentUserMachines: state.devices.currentUserMachines,
   // landingZones: state.devices.landingZones
-})
+});
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   openNotificationModal: (text: string) => dispatch(openNotificationModal('Notifications', text))
-})
+});
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(Machines);
