8c49433368cb9981be4442cb6a4c95734dda3d04
Tran Nguyen <trandnnguyen@gmail.com>
2020-02-20 12:19:21 -0800

    applied linting to Stations components

--- src/components/Stations/StationJob.tsx
+++ src/components/Stations/StationJob.tsx
@@ -1,88 +1,100 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { EJobRunningStatus } from '../../business/objects/job';
-import { Dispatch } from 'redux';
-import { IStore } from '../../business/objects/store';
-import { Dictionary } from '../../business/objects/dictionary';
-import { User } from '../../business/objects/user';
-import { Machine } from '../../business/objects/machine';
+import { Dictionary } from "../../business/objects/dictionary";
+import { Dispatch } from "redux";
+import { EJobRunningStatus } from "../../business/objects/job";
+import { IStore } from "../../business/objects/store";
+import { Machine } from "../../business/objects/machine";
+import { User } from "../../business/objects/user";
+import { connect } from "react-redux";
+import React from "react";
 
 type Props = {
   job: any;
   users: Dictionary<User>;
   machines: Dictionary<Machine>;
-}
-type State = {
-}
+};
+type State = {};
 
-class StationJob extends React.Component<Props, State>{
-  clockTimer: any;
-  constructor(props: Props){
+class StationJob extends React.Component<Props, State> {
+  constructor(props: Props) {
     super(props);
   }
-  public parseTime(seconds_elapsed:number, timeString:string = "", shortDuration:boolean = true):string{
-    if(seconds_elapsed >= 3600){
-      let hours:number = Math.floor(seconds_elapsed / 3600);
-      let hours_string: string = hours.toString();
-      if(hours < 10){
-        hours_string = '0' + hours;
+
+  public parseTime(
+    secondsElapsed: number,
+    timeString: string = "",
+    shortDuration: boolean = true
+  ): string {
+    if (secondsElapsed >= 3600) {
+      const hours: number = Math.floor(secondsElapsed / 3600);
+      let hoursString: string = hours.toString();
+      if (hours < 10) {
+        hoursString = "0" + hours;
       }
-      return this.parseTime(seconds_elapsed - (hours * 3600),`${hours_string}`, false);
-    }else{
+      return this.parseTime(
+        secondsElapsed - hours * 3600,
+        `${hoursString}`,
+        false
+      );
+    } else {
       let minutes = 0;
-      let seconds = seconds_elapsed;
-      let minutes_string = minutes.toString();
-      let seconds_string = seconds.toString();
-      if(seconds_elapsed >= 60){
-        minutes = Math.floor(seconds_elapsed / 60);
-        minutes_string = minutes.toString();
-        seconds = seconds_elapsed - (minutes * 60);
-        seconds_string = seconds.toString();
+      let seconds = secondsElapsed;
+      let minutesString = minutes.toString();
+      let secondsString = seconds.toString();
+      if (secondsElapsed >= 60) {
+        minutes = Math.floor(secondsElapsed / 60);
+        minutesString = minutes.toString();
+        seconds = secondsElapsed - minutes * 60;
+        secondsString = seconds.toString();
       }
-      if(seconds < 10){
-        seconds_string = '0' + seconds;
+      if (seconds < 10) {
+        secondsString = "0" + seconds;
       }
-      if(minutes < 10){
-        minutes_string = '0' + minutes;
+      if (minutes < 10) {
+        minutesString = "0" + minutes;
       }
-      if(shortDuration){
-        return `00:${minutes_string}:${seconds_string}`;
-      }else{
-        return timeString + `:${minutes_string}:${seconds_string}`;
+      if (shortDuration) {
+        return `00:${minutesString}:${secondsString}`;
+      } else {
+        return timeString + `:${minutesString}:${secondsString}`;
       }
     }
   }
-  render(){
+
+  render() {
     const { job } = this.props;
     let timer = job.run_time;
-    if(job.job_state === EJobRunningStatus.running){
-      timer = Math.floor(Math.floor(Date.now() / 1000) - job.last_updated) + job.run_time;
+    if (job.job_state === EJobRunningStatus.running) {
+      timer =
+        Math.floor(Math.floor(Date.now() / 1000) - job.last_updated) +
+        job.run_time;
     }
-    let time = this.parseTime(timer);
-    let launchPad = this.props.users[job.launch_pad] ? this.props.users[job.launch_pad].username : job.launch_pad;
-    let landingZone = this.props.machines[job.landing_zone] ? this.props.machines[job.landing_zone].machine_name : job.landing_zone;
-    return(
+    const time = this.parseTime(timer);
+    const launchPad = this.props.users[job.launch_pad]
+      ? this.props.users[job.launch_pad].username
+      : job.launch_pad;
+    const landingZone = this.props.machines[job.landing_zone]
+      ? this.props.machines[job.landing_zone].machine_name
+      : job.landing_zone;
+    return (
       <>
         <div className="log-column">
           <div className="station-job-info">
-                <div className="submitted-by">{landingZone}</div>
-                <div className="submitted-by">{launchPad}</div>
-                <div className="project-name">{job.name}</div>
-                <div className="job-time-taken">{time}</div>
+            <div className="submitted-by">{landingZone}</div>
+            <div className="submitted-by">{launchPad}</div>
+            <div className="project-name">{job.name}</div>
+            <div className="job-time-taken">{time}</div>
           </div>
         </div>
       </>
-    )
+    );
   }
 }
 
 const mapStateToProps = (state: IStore) => ({
   users: state.users.users,
   machines: state.machines.machines
-})
+});
 
-const mapDispatchToProps = (dispatch:Dispatch) => ({
-
-})
+const mapDispatchToProps = (dispatch: Dispatch) => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(StationJob);
