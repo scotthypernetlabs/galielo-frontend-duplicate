ecc5cfdc4fb4172effdb6a12f9af8866deb99ede
Tran Nguyen <trandnnguyen@gmail.com>
2020-03-10 11:46:33 -0700

    added Testing for Table component

---
+++ src/components/tests/Core/Table.test.tsx
@@ -0,0 +1,70 @@
+import { TableCell, TableHead, TableRow } from "@material-ui/core";
+import { mount } from "enzyme";
+import CustomTable, { TableHeader } from "../../Core/Table";
+import Job from "../../Jobs/Job";
+import React from "react";
+
+describe("CustomTable Component", () => {
+  const tableHeaders: TableHeader[] = [
+    { TableHeader1: "justify" },
+    { TableHeader2: "right" },
+    { TableHeader3: "center" }
+  ];
+  const tableBodyItems: JSX.Element[] = [
+    <TableRow key={1}>
+      <TableCell>1</TableCell>
+      <TableCell>2</TableCell>
+      <TableCell>3</TableCell>
+    </TableRow>,
+    <TableRow key={2}>
+      <TableCell>4</TableCell>
+      <TableCell>5</TableCell>
+      <TableCell>6</TableCell>
+    </TableRow>,
+    <TableRow key={3}>
+      <TableCell>7</TableCell>
+      <TableCell>8</TableCell>
+      <TableCell>9</TableCell>
+    </TableRow>
+  ];
+  const componentProps = {
+    tableHeaders,
+    tableBodyItems
+  };
+
+  const Wrapper = (props: any) => {
+    return <CustomTable {...componentProps} />;
+  };
+
+  const customTable = mount(<Wrapper />);
+
+  it("table headers should be correct", () => {
+    for (let i = 0; i < tableHeaders.length; i++) {
+      expect(
+        customTable
+          .find(TableCell)
+          .at(i)
+          .text()
+      ).toBe(Object.keys(tableHeaders[i])[0]);
+    }
+  });
+
+  it("table body should be correct", () => {
+    for (
+      let i = tableHeaders.length;
+      i < tableHeaders.length + tableBodyItems.length;
+      i++
+    ) {
+      expect(
+        customTable
+          .find(TableCell)
+          .at(i)
+          .text()
+      ).toBe((i - tableHeaders.length + 1).toString());
+    }
+  });
+
+  it("should match snapshot", () => {
+    expect(customTable).toMatchSnapshot();
+  });
+});
