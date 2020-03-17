d6d2f6eb2ee31fc959760abe886a6d6f04cf7c95
Tran Nguyen <trandnnguyen@gmail.com>
2020-03-17 13:42:20 -0700

    Lint

--- src/reducers/uiReducer.ts
+++ src/reducers/uiReducer.ts
@@ -1,27 +1,33 @@
-import { Reducer } from "redux";
-import { UIActions, FINISH_LOADING, NOTIFICATIONS_SELECTED, NOTIFICATIONS_UNSELECTED  } from "../actions/uiActions";
+import {
+  FINISH_LOADING,
+  NOTIFICATIONS_SELECTED,
+  NOTIFICATIONS_UNSELECTED,
+  UIActions
+} from "../actions/uiActions";
 import { IUIState } from "../business/objects/store";
+import { Reducer } from "redux";
 
 class UIState implements IUIState {
   constructor(
     public loadFinished: boolean = false,
     public notificationsSelected: boolean = false
-  ){
-
-  }
+  ) {}
 }
 
-const uiReducer: Reducer<UIState, UIActions> = (state = new UIState(), action:UIActions) => {
-  switch(action.type){
+const uiReducer: Reducer<UIState, UIActions> = (
+  state = new UIState(),
+  action: UIActions
+) => {
+  switch (action.type) {
     case FINISH_LOADING:
-      return Object.assign({}, state, {loadFinished: true});
+      return Object.assign({}, state, { loadFinished: true });
     case NOTIFICATIONS_SELECTED:
-      return Object.assign({}, state, {notificationsSelected: true});
+      return Object.assign({}, state, { notificationsSelected: true });
     case NOTIFICATIONS_UNSELECTED:
-      return Object.assign({}, state, {notificationsSelected: false});
+      return Object.assign({}, state, { notificationsSelected: false });
     default:
       return state;
   }
-}
+};
 
 export default uiReducer;
