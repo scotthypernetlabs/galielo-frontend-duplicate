5a59b6ccd3180358a0cb24e6465b02a993b525cf
Dan Luo <pichumy@gmail.com>
2020-04-03 12:43:23 -0700

    Readd in socket.close

--- src/data/interfaces/ISocket.ts
+++ src/data/interfaces/ISocket.ts
@@ -3,4 +3,5 @@ import io from "socket.io-client";
 export interface ISocket {
   emit: (channel: string, ...args: any[]) => Promise<any>;
   on: (channel: string, callback: Function) => void;
+  close: () => void;
 }
